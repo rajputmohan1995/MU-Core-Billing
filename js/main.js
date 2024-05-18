55b31b6af0b9a67985cc19eafbe1f2928b39e88a
Mohan Rajput <rajputmohan1995@gmail.com>
2024-05-18 22:35:46 +0530

    finish initial index page

--- js/main.js
+++ js/main.js
@@ -8,6 +8,23 @@ function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
 }
 
-$(document).ready(function(){
+$(document).ready(function () {
   $("#spanCurrentYear").text(new Date().getFullYear());
+});
+
+$("img.card-img-top").click(function (event) {
+  var imageTitle = $(event.target).attr("data-title");
+  var imgSrc = $(event.target).attr("src");
+
+  $("#how-it-works-img-viewer").modal("show");
+  $("#fullScreenImage").attr("src", imgSrc);
+  $("#exampleModalCenterTitle").text(imageTitle);
+});
+
+$("#btnViewTermsOfService").click(function () {
+  $("#popupTermsOfService").modal("show");
+});
+
+$("#btnViewPrivacyPolicy").click(function () {
+  $("#popupPrivacyPolicy").modal("show");
 });
