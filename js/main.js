be5165121ed34d9c1e6c89af1a665836fd63cd68
Mohan Rajput <44501000+rmohan20195@users.noreply.github.com>
2024-05-20 10:49:04 +0530

    fixes as per page insights

--- js/main.js
+++ js/main.js
@@ -1,13 +1,3 @@
-/* Open the sidenav */
-function openNav() {
-  document.getElementById("mySidenav").style.width = "100%";
-}
-
-/* Close/hide the sidenav */
-function closeNav() {
-  document.getElementById("mySidenav").style.width = "0";
-}
-
 $(document).ready(function () {
   $("#spanCurrentYear").text(new Date().getFullYear());
 });
